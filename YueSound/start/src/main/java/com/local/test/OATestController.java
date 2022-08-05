abbc75e9114eb5a86562eb3204a1dc853ef0c6f3
huheshuai <1903954476@qq.com>
2022-08-05 14:39:19 +0800

    去除多余文件

--- YueSound/start/src/main/java/com/local/test/OATestController.java
+++ YueSound/start/src/main/java/com/local/test/OATestController.java
@@ -17,52 +17,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/integrateThd")
 public class OATestController {
-    /*
-   fdKey(string)
-   附件的关键字，富文本模式下为"fdAttachment"，表单模式下为附件控件的id
-   * */
-    private static final String OA_KM_FD_KEY_START="<fdKey>" ;
-    private static final String OA_KM_FD_KEY_END="</fdKey>" ;
-    /*
-    fdFileName(string)
-    * 附件文件名
-    * */
-    private static final String OA_KM_FD_FILENAME_START="<fdFileName>" ;
-    private static final String OA_KM_FD_FILENAME_END="</fdFileName>" ;
-    /*
-   fdAttachment (字节数组 byte[])
-   * 附件内容，格式为字节编码
-   * */
-    private static final String OA_KM_FD_ATTACHMENT_START="<fdAttachment>" ;
-    private static final String OA_KM_FD_ATTACHMENT_END="</fdAttachment>" ;
-    private static final String OA_ATTACHMENT_FROMS_START="<attachmentForms>" ;
-    private static final String OA_ATTACHMENT_FROMS_END="</attachmentForms>" ;
-    private static final String OA_ATTACHMENT_VALUES_START="<attachmentValues>" ;
-    private static final String OA_ATTACHMENT_VALUES_END="</attachmentValues>" ;
-    private static final String OA_AUTH_AREA_ID_START="<authAreaId>" ;
-    private static final String OA_AUTH_AREA_ID_END="</authAreaId>" ;
-    private static final String OA_DOC_CONTENT_START="<docContent>" ;
-    private static final String OA_DOC_CONTENT_END="</docContent>" ;
-    private static final String OA_DOC_CREATOR_START="<docCreator>" ;
-    private static final String OA_DOC_CREATOR_END="</docCreator>" ;
-    private static final String OA_DOC_PROPERTY_START="<docProperty>" ;
-    private static final String OA_DOC_PROPERTY_END="</docProperty>" ;
-    private static final String OA_DOC_STATUS_START="<docStatus>" ;
-    private static final String OA_DOC_STATUS_END="</docStatus>" ;
-    private static final String OA_DOC_SUBJECT_START="<docSubject>" ;
-    private static final String OA_DOC_SUBJECT_END="</docSubject>" ;
-    private static final String OA_FDID_START="<fdId>" ;
-    private static final String OA_FDID_END="</fdId>" ;
-    private static final String OA_FD_KEYWORD_START="<fdKeyword>" ;
-    private static final String OA_FD_KEYWORD_END="</fdKeyword>" ;
-    private static final String OA_FD_SOURCE_START="<fdSource>" ;
-    private static final String OA_FD_SOURCE_END="</fdSource>" ;
-    private static final String OA_FD_TEMPLATEID_START="<fdTemplateId>" ;
-    private static final String OA_FD_TEMPLATEID_END="</fdTemplateId>" ;
-    private static final String OA_FLOW_PARAM_START="<flowParam>" ;
-    private static final String OA_FLOW_PARAM_END="</flowParam>" ;
-    private static final String OA_FROM_VALUES_START="<formValues>" ;
-    private static final String OA_FROM_VALUES_END="</formValues>" ;
+
     @PostMapping("/todoReceive")
     public String todoReceive(@RequestBody String json){
         Map returnMap=new HashMap();
